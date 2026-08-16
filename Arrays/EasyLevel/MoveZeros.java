da716e2ab64b9133252e2dc92e62d0c7ebaf870e
Aditigithub-acc <aditi456x@gmail.com>
2026-08-16 12:15:49 +0530

    Move zeros to end

--- Arrays/EasyLevel/MoveZeros.java
+++ Arrays/EasyLevel/MoveZeros.java
@@ -1,27 +1,18 @@
 package EasyLevel;
-
-public class MoveZeros {
-
-    public static void main(String[] args) {
-
-        int[] arr = {0, 1, 0, 3, 12};
-
-        int j = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i] != 0) {
-
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-
+public class MoveZeros{
+    public static void main(String[] args){
+        int[] arr={0,12,0,5,0,7,9,0,3};
+        int j=0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] !=0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
                 j++;
             }
         }
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        for(int i=0; i<arr.length;i++){
+            System.out.println(arr[i] + " ");
         }
     }
 }
