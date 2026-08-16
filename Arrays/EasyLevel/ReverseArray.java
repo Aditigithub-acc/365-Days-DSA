04ba07be7adb794d76ea701860c35208d11a6ccb
Aditigithub-acc <aditi456x@gmail.com>
2026-08-16 14:03:42 +0530

    Reverse of array

--- Arrays/EasyLevel/ReverseArray.java
+++ Arrays/EasyLevel/ReverseArray.java
@@ -1,17 +1,18 @@
 package EasyLevel;
-
-public class ReverseArray {
-    public static void main(String[] args){
-        int[] arr={1,2,3,4,5};
-        int Left=0;
-        int Right=arr.length-1;
-        for(int i=0; i<arr.length;i++){
-            int temp = arr[Left];
-            arr[Left] = arr[Right];
-            arr[Right] = temp;
-            Left++;
-            Right--;
-        }
-        System.out.println("Reverse of an array is: "+ Right);
+public class ReverseArray{
+public static void main(String[] args){
+    int[] arr={1,2,3,4};
+    int left=0;
+    int right=arr.length -1;
+    while(left<right){
+        int temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+        left++;
+        right--;
     }
+    for(int i=0; i<arr.length;i++){
+        System.out.println(arr[i]);
+    }
+}
 }
